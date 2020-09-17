7517133c385e147d38c82a89431aa7968d034ac0
Julius Karl <julius.karl.macrohon@gmail.com>
2020-09-17 19:16:20 +1200

    Stateless Widget (For Hot Restart)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-        home: Scaffold(
+void main() => runApp(
+    MaterialApp(theme: ThemeData(fontFamily: 'Questrial'), home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFFAFAFA),
         title: Image.asset('assets/img/Syno-AppBar.png',
@@ -9,17 +14,18 @@ void main() => runApp(MaterialApp(
         centerTitle: true,
       ),
       body: Center(
-        child: Text('Hello Ninjas!',
+        child: Text('Greetings Earth!',
             style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'OpenSans',
-            )),
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.grey[600],
+                fontFamily: 'Questrial')),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('Click'),
       ),
-    )));
+    );
+  }
+}
