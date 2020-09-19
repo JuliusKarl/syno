bc0c8d2b73d3c1de5891e92559948d4a48e91a5b
Julius Karl <julius.karl.macrohon@gmail.com>
2020-09-19 22:29:04 +1200

    Stateful Widget

--- lib/components.dart
+++ lib/components.dart
@@ -1,39 +1,34 @@
 import 'package:flutter/material.dart';
 
-var mainButton = Container(
-    width: 200.0,
-    height: 200.0,
-    child: new RawMaterialButton(
-      fillColor: white,
-      shape: new CircleBorder(),
-      elevation: 2.0,
-      child: Image.asset('assets/img/Syno-Button.png'),
-      onPressed: () {
-        print("Pressed!");
-      },
-    ));
+// Widgets
+class MainButton extends StatefulWidget {
+  String message;
 
-var drawer = Drawer(
-  child: ListView(
-    padding: EdgeInsets.zero,
-    children: <Widget>[
-      Container(
-          height: 100.0,
-          child: DrawerHeader(
-            child:
-                Text('Recent', style: TextStyle(fontSize: 30.0, color: grey)),
-          )),
-      ListTile(
-        title: Text('Item 1'),
-        onTap: () {},
-      ),
-      ListTile(
-        title: Text('Item 2'),
-        onTap: () {},
-      ),
-    ],
-  ),
-);
+  MainButton(message) {
+    this.message = message;
+  }
+  @override
+  _MainButtonState createState() => _MainButtonState();
+}
 
+class _MainButtonState extends State<MainButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 200.0,
+        height: 200.0,
+        child: new RawMaterialButton(
+          fillColor: white,
+          shape: new CircleBorder(),
+          elevation: 2.0,
+          child: Image.asset('assets/img/Syno-Button.png'),
+          onPressed: () {
+            print(widget.message);
+          },
+        ));
+  }
+}
+
+//  Styling
 var white = Color(0xFFFAFAFA);
 var grey = Color(0xFF555555);
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,13 @@ import 'components.dart';
 void main() => runApp(
     MaterialApp(theme: ThemeData(fontFamily: 'Questrial'), home: Home()));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int test = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,9 +24,28 @@ class Home extends StatelessWidget {
         ),
         body: Container(
             padding: EdgeInsets.fromLTRB(0, 0, 0, 80.0),
-            child: Center(child: mainButton)));
+            child: Center(child: MainButton("Pressed!"))));
   }
 }
 
-var white = Color(0xFFFAFAFA);
-var grey = Color(0xFF555555);
+var drawer = Drawer(
+  child: ListView(
+    padding: EdgeInsets.zero,
+    children: <Widget>[
+      Container(
+          height: 100.0,
+          child: DrawerHeader(
+            child:
+                Text('Recent', style: TextStyle(fontSize: 30.0, color: grey)),
+          )),
+      ListTile(
+        title: Text('Item 1'),
+        onTap: () {},
+      ),
+      ListTile(
+        title: Text('Item 2'),
+        onTap: () {},
+      ),
+    ],
+  ),
+);
